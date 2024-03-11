e2e6806ba9204efe9c47993a844001d2775e37cd
yizigezi <54109315+yizigezi@users.noreply.github.com>
2024-03-12 00:16:21 +0800

    coraos.fs.type.file 文件基础类型'File'更新

--- coraos/fs/type/file.py
+++ coraos/fs/type/file.py
@@ -1,2 +1,14 @@
+import os, mimetypes, time
+
 class File:
-    def __init__(self):
+    '''
+    File type
+    Basic type of file.
+    '''
+    def __init__(self, path: str):
+        self.Path = os.path.abspath(path)
+        self.Type = mimetypes.guess_type(self.path)[0]
+        stats = os.stat(self.path)
+        self.Size = stats.st_size
+        filetime = time.localtime(stats.st_mtime)
+        self.CreatedTime = time.strftime("%Y-%M-%d", filetime)
